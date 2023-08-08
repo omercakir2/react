c4ce3d7b4c85bfb3d785f49c1b3b00dc644f8d92
omercakir2 <omercakir2323@gmail.com>
2023-08-08 19:28:46 +0300

    little things changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Merhaba React</h1>
-    </div>
-  );
+const yas = 18
+const tf = true
+const isim = "omer"
+
+
+class App extends Component {
+
+  render() {
+    return (
+
+      <div className='App'>
+        <h1 className='Welcome'>Hosgeldin </h1>
+        <h1> {yas} </h1>
+        <h1>  {isim} </h1>
+
+        <div>
+          {
+            tf ? <p>Kayitli</p>
+              : null
+
+          }
+        </div>
+
+
+      </div>
+
+
+
+    );
+  }
 }
 
 export default App;
